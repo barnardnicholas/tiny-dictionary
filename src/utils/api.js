408c2e02912758e1718623474c078d8c5a823c92
barnardnicholas <barnard.nicholas@googlemail.com>
2020-02-15 14:29:51 +0000

    test env

--- src/utils/api.js
+++ src/utils/api.js
@@ -15,7 +15,7 @@ const wordsApi = axios.create({
 export const getDefinitions = word => {
   console.log(process.env.X_RAPIDAPI_HOST);
   console.log(process.env.X_RAPIDAPI_KEY);
-  console.dir(process.env);
+  console.dir(process);
 
   return wordsApi.get(word).then(response => {
     return {
